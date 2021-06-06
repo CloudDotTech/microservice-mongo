4fffb4d32c056298cca342896ae03f1e6e33764c
First Last <email.mail.com>
2021-06-05 20:51:58 -0500

    first commit

---
+++ src/main/java/com/cloud/microservicemongo/MicroserviceMongoApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.microservicemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/microservicemongo/MicroserviceMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.microservicemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
